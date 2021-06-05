e6429debac7174fffd8640c2aea3fd6ba5fe1a81
zexho <zouzhihao994@gmail.com>
2021-06-05 15:54:52 +0800

    update dependency

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess!");
     let mut guess = String::new();
-    let secret_number = rand.thred_rng().gen_range(1..101);
+    let secret_number = rand::thread_rng().gen_range(1..101);
     println!("The secret number is {}",secret_number);
     // 获取键盘输入
     io::stdin().read_line(&mut guess).expect("Failed to read line");
