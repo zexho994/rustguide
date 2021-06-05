65cf933fa8c5687c25520de1db6824c246c897db
zexho <zouzhihao994@gmail.com>
2021-06-05 21:36:47 +0800

    ownership done

---
+++ ownership/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // let s = String::from("world");
+    let s = "hello";
+    let s2 = s;
+    // let mut s = "hello";
+    // s2.push_str(",zexho");
+    println!("{}",s);
+    println!("{}",s2);
+}
