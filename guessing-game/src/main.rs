297d672e2283c90e68205451d6295002514d8b6f
zexho <zouzhihao994@gmail.com>
2021-06-05 16:49:10 +0800

    add gitignore

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,13 +1,23 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess!");
+    println!("[Game]: Guess the number!");
     let mut guess = String::new();
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("The secret number is {}",secret_number);
+
     // 获取键盘输入
+    println!("Please input your guess!");
     io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("You guessed : {}", guess);
+    let guess:u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
+    // compare secret and guess
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win"),
+    }
 }
