323542353384db52cd52390c215f35289f46219b
zexho <zouzhihao994@gmail.com>
2021-06-07 12:58:20 +0800

    refactor

--- rust_panic/src/main.rs
+++ rust_panic/src/main.rs
@@ -2,6 +2,12 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
+    // ver_panic();
+    define_error_msg();
+}
+
+#[warn(dead_code)]
+fn file_panic() {
     let f = File::open("hello.txt");
 
     let f = match f {
@@ -14,4 +20,21 @@ fn main() {
             other_error => panic!("Problem opening file: {:?}", error),
         },
     };
+}
+
+fn ver_panic() {
+    let mut vec = vec![1, 2, 3];
+    vec.insert(1, 4);
+    assert_eq!(vec, [1, 4, 2, 3]);
+    vec.insert(4, 5);
+    assert_eq!(vec, [1, 4, 2, 3, 5]);
+}
+
+fn define_error_msg() {
+    let x = false;
+    assert!(x, "x is false");
+
+    let a = 3;
+    let b = 4;
+    assert!(a > b, "a is less b");
 }
